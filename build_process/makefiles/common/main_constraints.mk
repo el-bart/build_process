58fb359ae67ec0910d9b5229134bbdcf5ca12aa7
el-bart <el-bart@tweety>
2009-01-19 17:42:12 +0100

    moved profiles into separete files; updated release notes and todo list;
    added:
      build_process/makefiles/common/profiles/
      build_process/makefiles/common/profiles/debug.mk
      build_process/makefiles/common/profiles/doc.mk
      build_process/makefiles/common/profiles/mtest.mk
      build_process/makefiles/common/profiles/profile.mk
      build_process/makefiles/common/profiles/release.mk
      build_process/makefiles/common/profiles/test.mk
    modified:
      RELEASE_NOTES
      TODO
      build_process/makefiles/common/dirs.mk
      build_process/makefiles/common/main_constraints.mk
      build_process/makefiles/common/main_macros.mk
      build_process/makefiles/common/targets.mk

--- build_process/makefiles/common/main_constraints.mk
+++ build_process/makefiles/common/main_constraints.mk
@@ -1,14 +1,6 @@
 # is any profile set?
-ifneq ($(PROFILE),test)
-ifneq ($(PROFILE),mtest)
-ifneq ($(PROFILE),debug)
-ifneq ($(PROFILE),release)
-ifneq ($(PROFILE),profile)
-ifneq ($(PROFILE),doc)
-$(error PROFILE not set (aviable are: debug, release, test, mtest, profile, doc))
-endif # PROFILE=doc
-endif # PROFILE=profile
-endif # PROFILE=release
-endif # PROFILE=debug
-endif # PROFILE=mtest
-endif # PROFILE=test
+PROFILES_LIST:=$(shell cd "$(MAKEFILES_PROFILES_BASE_DIR)" && ls | sed 's:\.mk::g' | xargs echo)
+
+ifneq ($(PROFILE),$(findstring $(PROFILE),$(PROFILES_LIST)))
+$(error PROFILE not set (aviable are: $(PROFILES_LIST)))
+endif # PROFILE not set
