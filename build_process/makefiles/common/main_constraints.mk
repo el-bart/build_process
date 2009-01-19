a85741b01a2fa52e3d783e09096cfb27290d7bb5
el-bart <el-bart@tweety>
2009-01-19 18:07:53 +0100

    toolchian selection added;
    added:
      build_process/makefiles/common/toolchains/
      build_process/makefiles/common/toolchains/local.mk
    modified:
      RELEASE_NOTES
      TODO
      build_process/makefiles/basic_mks/ProjectMakefile
      build_process/makefiles/common/dirs.mk
      build_process/makefiles/common/main_constraints.mk
      build_process/makefiles/common/tools.mk
      build_process/templates/config.mk

--- build_process/makefiles/common/main_constraints.mk
+++ build_process/makefiles/common/main_constraints.mk
@@ -1,6 +1,15 @@
+#
 # is any profile set?
+#
 PROFILES_LIST:=$(shell cd "$(MAKEFILES_PROFILES_BASE_DIR)" && ls | sed 's:\.mk::g' | xargs echo)
-
 ifneq ($(PROFILE),$(findstring $(PROFILE),$(PROFILES_LIST)))
 $(error PROFILE not set (aviable are: $(PROFILES_LIST)))
 endif # PROFILE not set
+
+#
+# is any toolchain set?
+#
+TOOLCHAINS_LIST:=$(shell cd "$(MAKEFILES_TOOLCHAINS_BASE_DIR)" && ls | sed 's:\.mk::g' | xargs echo)
+ifneq ($(TC),$(findstring $(TC),$(TOOLCHAINS_LIST)))
+$(error toolchain (TC) not set (aviable are: $(TOOLCHAINS_LIST)))
+endif # TC not set
