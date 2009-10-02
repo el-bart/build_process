4873a33ed108704ba2a2675c712768257205ab61
bartek szurgot <el-bart@tweety>
2009-10-02 20:05:15 +0200

    simplified dependencies for some compoenents. added dynamic compile flags test for release mode of Lib1.
    removed:
      test_app/app/features/deps/debug_dynamic
      test_app/app/features/deps/mtest_dynamic@
      test_app/app/features/deps/profile_dynamic@
      test_app/app/features/deps/release_dynamic@
      test_app/app/features/deps/test_dynamic@
      test_app/app2/features/deps/debug
      test_app/app2/features/deps/mtest
      test_app/app2/features/deps/profile
      test_app/app2/features/deps/release
      test_app/app2/features/deps/test
      test_app/lib3/features/deps/debug
      test_app/lib3/features/deps/mtest
      test_app/lib3/features/deps/profile
      test_app/lib3/features/deps/release
      test_app/lib3/features/deps/test
    added:
      test_app/lib1/features/deps/release_dynamic_compile
    renamed:
      test_app/lib2/features/deps/common_dynamic_link => test_app/lib1/features/deps/common_dynamic_link
    modified:
      test_app/lib1/Lib1/Foo.cpp
      test_app/lib1/features/deps/common_dynamic_link

--- test_app/lib1/Lib1/Foo.cpp
+++ test_app/lib1/Lib1/Foo.cpp
@@ -1,5 +1,19 @@
 #include "Lib1/Foo.hpp"
 
+#ifdef NDEBUG
+
+#ifndef LIB1_IN_RELEASE_MODE_ONLY
+#error "flag LIB1_IN_RELEASE_MODE_ONLY has NOT been set in release mode"
+#endif
+
+#else
+
+#ifdef LIB1_IN_RELEASE_MODE_ONLY
+#error "flag LIB1_IN_RELEASE_MODE_ONLY HAS been set in non-release mode"
+#endif
+
+#endif
+
 void Foo::resetTo(int v)
 {
     for(unsigned int i=0; i<size(); ++i)
