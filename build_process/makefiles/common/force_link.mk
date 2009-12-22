3c5e3e0648dfdfca96228d931d649b8394a86cc0
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-12-22 11:22:04 +0100

    fixed anomaly od rebuilding force-linked object from debug when test profile has been removed.
    removed:
      build_process/makefiles/common/force_link.mk
    modified:
      build_process/makefiles/common/basic_flags.mk
      build_process/makefiles/common/pre_build.mk

--- build_process/makefiles/common/force_link.mk
+++
@@ -1,9 +0,0 @@
-# target's short name
-.PHONY: ForceLink
-ForceLink: $(GEN_INCLUDES_DIR)/BuildProcess/ForceLink.hpp
-
-# rule to create proper file
-$(GEN_INCLUDES_DIR)/BuildProcess/ForceLink.hpp:
-	@echo "MKHPP BuildProcess/ForceLink.hpp"
-	@mkdir -p "$(GEN_INCLUDES_DIR)/BuildProcess/"
-	@ln -s "$(CODE_BASE_DIR)/BuildProcess/ForceLink.hpp" "$@"
--- build_process/makefiles/common/pre_build.mk
+++ build_process/makefiles/common/pre_build.mk
@@ -1,8 +1,6 @@
 # include all required features
-include $(MAKEFILES_COMMON_BASE_DIR)/force_link.mk
 include $(MAKEFILES_COMMON_BASE_DIR)/version_check.mk
 
 # force doing some extra stuff before building starts
 .PHONY: pre_build
-pre_build: ForceLink
 pre_build: $(BUILD_PROCESS_VERSION_TIMESTAMP)
