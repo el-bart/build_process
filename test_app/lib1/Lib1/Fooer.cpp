54ea9f12cc1b61bdae3b00a541fbbc4eaa7f0a74
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-27 08:34:56 +0100

    implemented and tested component-local flags.
    added:
      test_app/lib1/features/deps/common_dynamic_compile_local
      test_app/lib1/features/deps/test_dynamic_compile_local
      test_app/lib2/Lib2/testNonLolcalFlagsAbsence.cpp
    modified:
      RELEASE_NOTES
      TODO
      build_process/scripts/getdeps
      build_process/templates/features/deps/HOWTO.txt
      test_app/lib1/Lib1/Fooer.cpp
      test_app/lib1/Lib1/Fooer.mt.cpp
      test_app/lib1/Lib1/testset1.t.cpp

--- test_app/lib1/Lib1/Fooer.cpp
+++ test_app/lib1/Lib1/Fooer.cpp
@@ -6,6 +6,11 @@
 #error "SOME_PROJECT_SPECIFIC_OPTION not set - project-specific flags not working"
 #endif
 
+#ifndef LIB1__LOCAL_FLAG
+#error "LIB1__LOCAL_FLAG not set"
+#endif
+
+
 void Fooer::reset(void)
 {
     resetTo(0);
