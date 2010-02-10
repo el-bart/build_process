11d84d53046d9fd3c3f2b3c2d0b70bb23910f1f3
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-02-10 16:50:40 +0100

    intel-toolchain (icc) - basic version (probably needs some improvements).
    added:
      build_process/makefiles/common/toolchains/intel-flags.mk
      build_process/makefiles/common/toolchains/intel.mk

---
+++ build_process/makefiles/common/toolchains/intel.mk
@@ -0,0 +1,11 @@
+CC:=icc
+CXX:=icc
+AR:=xiar
+# TODO: can ranlib and nm be used directl?
+RANLIB:=ranlib
+NM:=nm
+#LD:=xild
+LD:=$(CXX)
+# TODO: can strop be used here?
+STRIP:=strip
+DOXYGEN:=doxygen
