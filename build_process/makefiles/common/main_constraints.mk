f2871bdfb3c0ea5b863b8fbef62e15143d5badc5
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-02-11 11:02:09 +0100

    fixed display of available toolchains.
    modified:
      RELEASE_NOTES
      build_process/makefiles/common/main_constraints.mk

--- build_process/makefiles/common/main_constraints.mk
+++ build_process/makefiles/common/main_constraints.mk
@@ -9,7 +9,7 @@ endif # PROFILE not set
 #
 # is any toolchain set?
 #
-TOOLCHAINS_LIST:=$(shell cd "$(MAKEFILES_TOOLCHAINS_BASE_DIR)" && ls | sed 's:\.mk::g' | xargs echo)
+TOOLCHAINS_LIST:=$(shell cd "$(MAKEFILES_TOOLCHAINS_BASE_DIR)" && ls | grep -v -- '-flags.mk' | sed 's:\.mk::g' | xargs echo)
 ifneq ($(TC),$(findstring $(TC),$(TOOLCHAINS_LIST)))
 $(error toolchain (TC) not set (aviable are: $(TOOLCHAINS_LIST)))
 endif # TC not set
