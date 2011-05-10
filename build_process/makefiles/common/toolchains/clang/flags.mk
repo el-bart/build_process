8a184c7ce4aeed4b9b20da23385f85f6ae802ece
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-05-10 18:06:13 +0200

    moved back to -O3, since -O4 causes problems
    modified:
      build_process/makefiles/common/toolchains/clang/flags.mk

--- build_process/makefiles/common/toolchains/clang/flags.mk
+++ build_process/makefiles/common/toolchains/clang/flags.mk
@@ -1,11 +1,14 @@
 #
 # NOTES
 # -Qunused-arguments suppresses warning on unused compilation flags - should not bee needed in the future.
+# -Werror            should be enabled for the release mode as soon as clang will be more standard-compliant.
+# -O3                though clang supports -O4 as the more aggressive optimisations it does not seems to work.
 #
 
 # compiler flags
 CMN_FLAGS:=-Wall -Wextra -pipe -Qunused-arguments
-OPT_FLAGS:=-O4 -DNDEBUG -Werror -march=native
+#OPT_FLAGS:=-O3 -DNDEBUG -Werror -march=native
+OPT_FLAGS:=-O3 -DNDEBUG -march=native
 DBG_FLAGS:=-g3
 PRF_FLAGS:=-g $(OPT_FLAGS) -pg
 
